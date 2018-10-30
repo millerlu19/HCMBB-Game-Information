623ecebb1adbc78acc6cd31c31941e517a94d997
Lucas Miller <millerlu19@hanover.edu>
2018-10-30 15:39:19 -0400

    created "set" functions for my instance variables in my HanoverGame class; added get/set location functions; initialized instance variables; created PageUrl.txt

--- HanoverGame.py
+++ HanoverGame.py
@@ -1,17 +1,42 @@
 # HanoverGame.py
 
+from bs4 import BeautifulSoup
 class HanoverGame:
     def __init__(self, page):
         self.page = page
+        self.location = setLocation()
+        self.opponent = setOpponent()
+        self.HanoverScore = setHanoverScore()
+        self.OpponentScore = setOpponentScore()
+        self.date = setDate()
 
-    def getOpponent():
+    def setLocation(self):
+        # get location info from self.page
         return None
 
-    def getHanoverScore():
+    def setOpponent(self):
         return None
 
-    def getOpponentScore():
+    def setHanoverScore(self):
         return None
 
-    def getDate():
+    def setOpponentScore(self):
         return None
+
+    def setDate(self):
+        return None
+
+    def getLocation(self):
+        return None
+
+    def getOpponent(self):
+        return self.opponent
+
+    def getHanoverScore(self):
+        return self.HanoverScore
+
+    def getOpponentScore(self):
+        return self.OpponentScore
+
+    def getDate(self):
+        return self.date
