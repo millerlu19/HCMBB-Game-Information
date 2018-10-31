81fba8373c9b70bc9217673dcff2978a74e07037
Lucas Miller <millerlu19@hanover.edu>
2018-10-31 15:26:15 -0400

    Entered code for setHanoverScore() and setOpponentScore() functions in HanoverGame.py

--- HanoverGame.py
+++ HanoverGame.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 class HanoverGame:
+
     def __init__(self, page):
         self.page = page
         self.location = setLocation()
@@ -18,10 +19,26 @@ class HanoverGame:
         return None
 
     def setHanoverScore(self):
-        return None
+        soup = BeautifulSoup(self.page.text, 'html.parser')
+        if self.location == "Home":
+            HanoverHomeScore = soup.find(class_='team-score home')
+            HanoverHomeTotal = list(HanoverHomeScore.children)[0]
+            return HanoverHomeTotal
+        else:
+            HanoverAwayScore = soup.find(class_='team-score visitor')
+            HanoverAwayTotal = list(HanoverAwayScore.children)[0]
+            return HanoverAwayTotal
 
     def setOpponentScore(self):
-        return None
+        soup = BeautifulSoup(self.page.text, 'html.parser')
+        if self.location == "Home":
+            OpponentHomeScore = soup.find(class_='team-score home')
+            OpponentHomeTotal = list(OpponentHomeScore.children)[0]
+            return OpponentHomeTotal
+        else:
+            OpponentAwayScore = soup.find(class_='team-score visitor')
+            OpponentAwayTotal = list(OpponentAwayScore.children)[0]
+            return OpponentAwayTotal
 
     def setDate(self):
         return None
