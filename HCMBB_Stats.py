b2ab7a6fee82285bf3591315ecb19fac62446543
Lucas Miller <millerlu19@hanover.edu>
2019-01-27 15:56:47 -0500

    setting up program to implement PageUrls.txt and HanoverGame class

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -7,17 +7,21 @@ import HanoverGame.py
 # helper function to read all of the urls from PageUrl.txt
 def get_game_urls():
     PageUrls = open('PageUrl.txt', 'r')
-    return PageUrls
+    game_pages = []
+    for url in game_urls:
+        game_pages.append(HanoverGame(get_game_page(url)))
+    return game_pages
 
 # helper function that takes the url and opens the page through a get request and returns the page
 def get_game_page(url):
     return game_pages[url]
 
+# main()
+# Creates a long list of the box score urls for each game played
+# Create a HanoverGame object for each game page url
+    #
 def main():
     game_urls = get_game_urls()
-    game_pages = []
-    for url in game_urls:
-        game_pages.append(HanoverGame(get_game_page(url)))
-    print(HanoverGame.__init__)
+    print(game_urls)
 
 main()
