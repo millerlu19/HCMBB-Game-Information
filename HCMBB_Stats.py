b8d3233026c9fd3cd9cec9b129bca3ab928b6ff2
Lucas Miller <millerlu19@hanover.edu>
2019-01-27 15:11:48 -0500

    commit for attempt to open PageUrl.txt file

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -6,6 +6,7 @@ import HanoverGame.py
 
 # helper function to read all of the urls from PageUrl.txt
 def get_game_urls():
+    PageUrls = open('PageUrl.txt', 'r')
     return PageUrls
 
 # helper function that takes the url and opens the page through a get request and returns the page
@@ -13,7 +14,6 @@ def get_game_page(url):
     return game_pages[url]
 
 def main():
-    PageUrls = open('PageUrl.txt', 'r')
     game_urls = get_game_urls()
     game_pages = []
     for url in game_urls:
