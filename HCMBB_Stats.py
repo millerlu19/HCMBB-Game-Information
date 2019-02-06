b7159b93b73393a5490fb02677de9d3997a7781d
Lucas Miller <millerlu19@hanover.edu>
2019-02-06 01:34:44 -0500

    attempting to change get_game_urls() function for the return type to be a dictionary

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -12,33 +12,47 @@ def get_game_urls():
     """
     PageUrls = open('PageUrl.txt', 'r')
     Txt_File_List = PageUrls.readlines()
-    Urls_List = []
-    for url in Txt_File_List:
-        url = url.rstrip()
-        if not url.startswith('#'):
-            Urls_List.append(url)
+    #Urls_List = []
+    #for url in Txt_File_List:
+    #    url = url.rstrip()
+    #    if not url.startswith('#'):
+    #        Urls_List.append(url)
 
-    Urls_List_18 = Urls_List[:30]
-    Urls_List_19 = Urls_List[31:]
+    #Urls_List_18 = Urls_List[:30]
+    #Urls_List_19 = Urls_List[31:]
+
+    Urls_Dict = {}
+    for url in Txt_File_List:
+        k, v = url.strip().split('=')
+        Urls_Dict[k.strip()] = v.strip()
 
     PageUrls.close()
 
-    return Urls_List_18, Urls_List_19
+    #return Urls_List_18, Urls_List_19
+    return Urls_Dict
 
 # helper function that takes the url and opens the page through a get request and returns the page
-def get_game_page(url):
-    """Returns the info of the selected game's box score page from Hanover Presto Sports."""
+def get_game_page():
+    """Returns the specific game url for the game that is being selected."""
 
-    return game_pages[url]
+    return
+
+def create_game_objects():
+    """Returns the info collected from the HanoverGame class for the selected game."""
+
+    return
 
 # main()
 # Creates a long list of the box score urls for each game played
 # Create a HanoverGame object for each game page url
     # dictionary -> key: tuple (opponent, month & day, year), value: HanoverGame object
 def main():
-    Game_URLs_2018 = get_game_urls()[0]
-    print(Game_URLs_2018)
-    Game_URLs_2019 = get_game_urls()[1]
-    print(Game_URLs_2019)
+    #Game_Urls_2018 = get_game_urls()[0]
+    #print(Game_Urls_2018)
+    #Game_Urls_2019 = get_game_urls()[1]
+    #print(Game_Urls_2019)
+
+    Game_Urls = get_game_urls()
+    print(Game_Urls)
 
 main()
