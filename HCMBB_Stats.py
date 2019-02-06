c1229555fbc419c05cd0f3e23a8f51517394fd53
Lucas Miller <millerlu19@hanover.edu>
2019-02-05 19:30:33 -0500

    implemented docstrings to current functions in HCMBB_Stats.py

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -6,6 +6,7 @@ import HanoverGame.py
 
 # helper function to read all of the urls from PageUrl.txt
 def get_game_urls():
+    """Returns a list of all the urls of each Hanover games' box scores."""
     PageUrls = open('PageUrl.txt', 'r')
     game_pages = []
     for url in game_urls:
@@ -14,6 +15,7 @@ def get_game_urls():
 
 # helper function that takes the url and opens the page through a get request and returns the page
 def get_game_page(url):
+    """Returns the info of the selected game's box score page from Hanover Presto Sports."""
     return game_pages[url]
 
 # main()
