bf482148626619da279056a7e8eb596b5cad430d
Lucas Miller <millerlu19@hanover.edu>
2019-02-07 19:41:15 -0500

    created one big dictionary from get_urls_dict() with key=(opponent, date) and value=game url

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -26,21 +26,37 @@ def get_urls_list():
 
     return Urls_List_18, Urls_List_19
 
+# def get_urls_dict()
+# key = (opponent, date)
+# value = url
 def get_urls_dict():
     """
     Returns a list of all the urls of each Hanover games' box scores.
-    Two separate Lists for the 2017-2018 season and the 2018-2019 season.
+    Two separate Dictionaries for the 2017-2018 season and the 2018-2019 season.
     """
     PageUrls = open('PageUrl.txt', 'r')
 
     Urls_Dict = {}
     for line in PageUrls:
-        opp, link = line.split(' = ')
-        Urls_Dict = {opp: link}
+        line = line.strip()
+        print(line)
+        if not line.startswith('#') and not len(line) == 0:
+            opp, link = line.split(' = ')
+            date = link.split('/')[-1].split('_')[0]
+            opponent = (opp, date)
+            print(opponent)
+            Urls_Dict[opponent] = link
+            print(Urls_Dict)
+            print(len(Urls_Dict))
 
     PageUrls.close()
 
     return Urls_Dict
+
+# helper function that takes in the game url as a parameter and returns the date of the selected game played
+def get_game_date(url):
+    """Returns the date of the selected game played from its url"""
+
 
 # helper function that takes the url and opens the page through a get request and returns the page
 def get_game_page():
@@ -58,12 +74,12 @@ def create_game_objects():
 # Create a HanoverGame object for each game page url
     # dictionary -> key: tuple (opponent, month & day, year), value: HanoverGame object
 def main():
-    Urls_List_2018 = get_urls_list()[0]
-    print(Urls_List_2018)
-    Urls_List_2019 = get_urls_list()[1]
-    print(Urls_List_2019)
+    #Urls_List_2018 = get_urls_list()[0]
+    #print(Urls_List_2018)
+    #Urls_List_2019 = get_urls_list()[1]
+    #print(Urls_List_2019)
 
-    #Urls_Dict = get_urls_dict()
-    #print(Urls_Dict)
+    Urls_Dict = get_urls_dict()
+    print(Urls_Dict)
 
 main()
