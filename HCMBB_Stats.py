b97340391309739d88f54fb2e13b233af3cf6176
Lucas Miller <millerlu19@hanover.edu>
2019-02-11 13:51:13 -0500

    more work with get_game_page() function. cannot figure out how to re-enter opponent if the first entered input is not valid

--- HCMBB_Stats.py
+++ HCMBB_Stats.py
@@ -1,8 +1,11 @@
 # HCMBB_Stats.py
+from typing import Dict, Any
 
 import requests
 from bs4 import BeautifulSoup
-#import HanoverGame.py
+
+
+# import HanoverGame.py
 
 # def get_urls_dict()
 # key = (opponent, date)
@@ -33,42 +36,60 @@ def get_game_url_dict():
 
     return Game_Urls_2018, Game_Urls_2019
 
+
 # helper function that takes the url and opens the page through a get request and returns the page
-def get_game_page(Game_Urls_2019, Game_Urls_2018):
+def get_game_page(game_urls_2019, game_urls_2018):
     """Returns the specific game url for the game that is being selected."""
 
     filtered_dict = {}
     szn_input = input("Which Hanover Men's Basketball season would you like to view? (2019 or 2018) ")
     if szn_input == "2019":
         opp_input = input("Which game in this season would you like to view? (enter opponent) ")
-        for key, val in Game_Urls_2019.items():
+        for key, val in game_urls_2019.items():
             if opp_input == key[0]:
                 filtered_dict[key] = val
-        print(filtered_dict)
-        if len(filtered_dict) == 0:
+        if filtered_dict is None:
             print("Hanover did not play this team in this season. Please try again.")
-            get_game_page(Game_Urls_2019, Game_Urls_2018)
+            get_game_page(game_urls_2019, game_urls_2018)
         elif len(filtered_dict) == 1:
-            return filtered_dict[opp_input]
+            return filtered_dict
         else:
             return filtered_dict
+    elif szn_input == "2018":
+        opp_input = input("Which game in this season would you like to view? (enter opponent) ")
+        for key, val in game_urls_2018.items():
+            if opp_input == key[0]:
+                filtered_dict[key] = val
+        if filtered_dict is None:
+            print("Hanover did not play this team in this season. Please try again.")
+            get_game_page(game_urls_2019, game_urls_2018)
+        elif len(filtered_dict) == 1:
+            return filtered_dict
+        else:
+            return filtered_dict
+    else:
+        print("This season is not available in this application. Try Either '2019' or '2018.")
+        get_game_page(game_urls_2019, game_urls_2018)
+
 
 def create_game_objects():
     """Returns the info collected from the HanoverGame class for the selected game."""
 
     return
 
+
 # main()
 # Creates a long list of the box score urls for each game played
 # Create a HanoverGame object for each game page url
-    # dictionary -> key: tuple (opponent, month & day, year), value: HanoverGame object
+# dictionary -> key: tuple (opponent, month & day, year), value: HanoverGame object
 def main():
-    Game_Urls_2018 = get_game_url_dict()[0]
-    print(Game_Urls_2018)
-    Game_Urls_2019 = get_game_url_dict()[1]
-    print(Game_Urls_2019)
+    game_urls_2018 = get_game_url_dict()[0]
+    print(game_urls_2018)
+    game_urls_2019 = get_game_url_dict()[1]
+    print(game_urls_2019)
 
-    Game_Page = get_game_page(Game_Urls_2018, Game_Urls_2019)
-    print(Game_Page)
+    game_page = get_game_page(game_urls_2018, game_urls_2019)
+    print(game_page)
+
 
 main()
