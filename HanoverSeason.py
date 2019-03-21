799a88c95a944a09bd638345e21fd976ce4a4350
Lucas Miller <millerlu19@hanover.edu>
2019-03-21 17:59:10 -0400

    more initial setup for HanoverSeason class

--- HanoverSeason.py
+++ HanoverSeason.py
@@ -3,7 +3,27 @@
 class HanoverSeason:
 
     def __init__(self):
-        self.season = season
-        self.start_date = start_date
-        self.end_date = end_date
-        self.games = games
+        self.season = setSeason()
+        self.StartDate = setStartDate()
+        self.EndDate = setEndDate()
+        self.games = setGames()
+
+    def setSeason(self):
+
+    def setStartDate(self):
+
+    def setEndDate(self):
+
+    def setDate(self):
+
+    def getSeason(self):
+        return self.season
+
+    def getStartDate(self):
+        return self.StartDate
+
+    def getEndDate(self):
+        return self.EndDate
+
+    def getGames(self):
+        return self.games
