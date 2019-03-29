479ea4c7f9cad95739babdbf0c588d7a7ed2259a
Lucas Miller <millerlu19@hanover.edu>
2019-03-29 17:47:56 -0400

    fixed test variables to match HanoverGame class variables

--- test_hanoverGame.py
+++ test_hanoverGame.py
@@ -12,11 +12,12 @@ class TestHanoverGame(TestCase):
         self.assertEqual("Timken Gymnasium; Wooster, Ohio", vs_wheaton.location)
         self.assertEqual("March 1, 2019", vs_wheaton.date)
         self.assertEqual("Wheaton", vs_wheaton.opponent)
-        self.assertEqual("73", vs_wheaton.HanoverScore)
-        self.assertEqual("84", vs_wheaton.OpponentScore)
+        self.assertEqual("73", vs_wheaton.hanover_score)
+        self.assertEqual("84", vs_wheaton.opponent_score)
 
         # Test 2: Butler; 10/28/2017
         butler_url = "https://hanover.prestosports.com/sports/mbkb/2017-18/boxscores/20171028_m0c1.xml"
         vs_butler = HanoverGame.HanoverGame(butler_url)
         print(vs_butler)
         self.assertEqual("Indianapolis, Ind. -- Hinkle Fieldhouse", vs_butler.location)
+        self.assertEqual("October 28, 2017", vs_butler.date)
