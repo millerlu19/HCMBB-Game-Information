afce478a4e145ae1c817d3c9f8004b8a42d20151
Lucas Miller <millerlu19@hanover.edu>
2019-03-31 23:21:00 -0400

    started refactoring for set_opponent() method; created one helper function: get_opponent_string()

--- HanoverGame.py
+++ HanoverGame.py
@@ -33,16 +33,13 @@ class HanoverGame:
             if len(opponent_name[3]) < 4 or opponent_name[3] == "Kent":
                 if opponent_name[5] == "-":
                     opponent_name_list = opponent_name[3:7]
-                    opponent_name_string = " ".join(opponent_name_list)
-                    return opponent_name_string
+                    return self.get_opponent_string(opponent_name_list)
                 else:
                     opponent_name_list = opponent_name[3:6]
-                    opponent_name_string = " ".join(opponent_name_list)
-                    return opponent_name_string
+                    return self.get_opponent_string(opponent_name_list)
             elif len(opponent_name[3]) == 4:
                 opponent_name_list = opponent_name[3:5]
-                opponent_name_string = " ".join(opponent_name_list)
-                return opponent_name_string
+                return self.get_opponent_string(opponent_name_list)
             else:
                 opponent_name_string = opponent_name[3]
                 return opponent_name_string
@@ -50,19 +47,20 @@ class HanoverGame:
             if len(opponent_name[1]) < 4 or opponent_name[1] == "Kent":
                 if opponent_name[3] == "-":
                     opponent_name_list = opponent_name[1:5]
-                    opponent_name_string = " ".join(opponent_name_list)
-                    return opponent_name_string
+                    return self.get_opponent_string(opponent_name_list)
                 else:
                     opponent_name_list = opponent_name[1:4]
-                    opponent_name_string = " ".join(opponent_name_list)
-                    return opponent_name_string
+                    return self.get_opponent_string(opponent_name_list)
             elif len(opponent_name[3]) == 4:
                 opponent_name_list = opponent_name[1:3]
-                opponent_name_string = " ".join(opponent_name_list)
-                return opponent_name_string
+                return self.get_opponent_string(opponent_name_list)
             else:
                 opponent_name_string = opponent_name[1]
                 return opponent_name_string
+
+    def get_opponent_string(self, opponent_name):
+        opponent_name_string = " ".join(opponent_name)
+        return opponent_name_string
 
     def set_hanover_score(self):
         if self.is_home_game():
