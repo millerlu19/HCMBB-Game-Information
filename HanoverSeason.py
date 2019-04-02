14153e4710378d31439759fee8f96424a3f49a98
Lucas Miller <millerlu19@hanover.edu>
2019-04-02 18:22:30 -0400

    initialized set_season() method with helper function: season_from_user(); also created full_season_str() method in case I needed to label season as 2018-2019 instead of just 2019

--- HanoverSeason.py
+++ HanoverSeason.py
@@ -1,8 +1,9 @@
 # HanoverSeason.py
 
+import HCMBB_Stats
 import HanoverGame
 
-class HanoverSeason:
+class HanoverSeason(HCMBB_Stats, HanoverGame):
 
     def __init__(self):
         self.season = self.set_season()
@@ -11,7 +12,19 @@ class HanoverSeason:
         self.games = self.set_games()
 
     def set_season(self):
+        seasons_dict = HCMBB_Stats.get_game_url_dict()
+        seasons = list(seasons_dict.keys())
+        season = self.season_from_user(seasons)
+        return season
 
+    def season_from_user(self, seasons):
+        season_input = int(input("Which Hanover Men's Basketball season would you like to view? Seasons include: {}. ".format(seasons)))
+        return season_input
+
+    def full_season_string(self):
+        season = self.set_season()
+        full_season_str = str(season - 1) + "-" + str(season)
+        return full_season_str
 
     def set_start_date(self):
 
