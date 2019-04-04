fc34f7ebc6ac8ccec54fd3f26e82edf2d0d4ecdd
Lucas Miller <millerlu19@hanover.edu>
2019-04-03 21:38:26 -0400

    successfully ran both tests for HanoverSeason file

--- test_hanoverSeason.py
+++ test_hanoverSeason.py
@@ -6,7 +6,7 @@ class TestHanoverSeason(TestCase):
         import HanoverSeason
         import HCMBB_Stats
 
-        # Test 1: 2017-2018; 10/28/2017-3/2/2018
+        # Test 1: 2017-2018; 10/1/2017-3/31/2018
         season_year = 2018
         seasons_dict = HCMBB_Stats.get_game_url_dict()
         season_2018 = HanoverSeason.HanoverSeason(season_year, seasons_dict)
@@ -14,4 +14,13 @@ class TestHanoverSeason(TestCase):
         self.assertEqual("2017-2018", season_2018.season_id)
         self.assertEqual("10/1/2017", season_2018.start_date)
         self.assertEqual("3/31/2018", season_2018.end_date)
+
+        # Test 2: 2018-2019; 10/1/2018-3/31/2019
+        season_year = 2019
+        seasons_dict = HCMBB_Stats.get_game_url_dict()
+        season_2019 = HanoverSeason.HanoverSeason(season_year, seasons_dict)
+        print(season_2019)
+        self.assertEqual("2018-2019", season_2019.season_id)
+        self.assertEqual("10/1/2018", season_2019.start_date)
+        self.assertEqual("3/31/2019", season_2019.end_date)
 
