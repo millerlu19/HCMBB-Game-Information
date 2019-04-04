fde15ad48a069057356f6b110d2b02aa19673e75
Lucas Miller <millerlu19@hanover.edu>
2019-04-04 18:57:46 -0400

    completed all code for HCMBB_Text_Interface file; may need to refactor; still need to debug neutral site games

--- HCMBB_Text_Interface.py
+++ HCMBB_Text_Interface.py
@@ -12,6 +12,7 @@ def main():
     for year in seasons_list:
         print(HanoverSeason.HanoverSeason(year, seasons_dict).season_id)
     season_input = input("Select Season: ")
+
     season_year = int(season_input[-4:])
     # season_inp_dict = dictionary of each game in season {(opponent, date_id): url}
     season_inp_dict = seasons_dict[season_year]
@@ -23,11 +24,32 @@ def main():
         hanover_score = HanoverGame.HanoverGame(game_url).hanover_score
         opp_score = HanoverGame.HanoverGame(game_url).opponent_score
         if hanover_score > opp_score:
-            result = "W"
+            game_result = "W"
         else:
-            result = "L"
-        print(game_number, "->", game_date + ":", game_opp + ",", result, hanover_score, "-", opp_score)
+            game_result = "L"
+        print(game_number, "->", game_date + ":", game_opp + ",", game_result, hanover_score, "-", opp_score)
         game_number += 1
+    game_input = int(input("Select game number: "))
+
+    opp_list = HanoverSeason.HanoverSeason(season_year, seasons_dict).opponents_list
+    opp_name = opp_list[game_input-1]
+    game_keys = seasons_dict[season_year].keys()
+    for key in game_keys:
+        if key[0] == opp_name:
+            game_key = key
+    sel_game_url = seasons_dict[season_year][game_key]
+    sel_game_date = HanoverGame.HanoverGame(sel_game_url).date
+    sel_game_opp = HanoverGame.HanoverGame(sel_game_url).opponent
+    sel_game_hc_score = HanoverGame.HanoverGame(sel_game_url).hanover_score
+    sel_game_opp_score = HanoverGame.HanoverGame(sel_game_url).opponent_score
+    sel_game_location = HanoverGame.HanoverGame(sel_game_url).location
+    if sel_game_hc_score > sel_game_opp_score:
+        sel_game_result = "W"
+    else:
+        sel_game_result = "L"
+    print("Date:", sel_game_date, "\n")
+    print(sel_game_result + ",", "Hanover:", sel_game_hc_score, sel_game_opp + ":", sel_game_opp_score, "\n")
+    print("Game Location:", sel_game_location)
 
 
 main()
